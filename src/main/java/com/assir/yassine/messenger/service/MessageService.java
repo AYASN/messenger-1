198bf4267789ac3c86ab366fbf13c2a7486b123d
AYassine <assiryassine@gmail.com>
2019-11-09 10:12:43 +0100

    Build message service stubs

--- src/main/java/com/assir/yassine/messenger/service/MessageService.java
+++ src/main/java/com/assir/yassine/messenger/service/MessageService.java
@@ -1,20 +1,46 @@
 package com.assir.yassine.messenger.service;
 
+import com.assir.yassine.messenger.database.DatabaseClass;
 import com.assir.yassine.messenger.model.Message;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MessageService {
 
-    public List<Message> getAllMessages() {
+    private Map<Long, Message> messages = DatabaseClass.getMessages();
 
-        Message m1 = new Message(1L, "Hello world", "assir");
-        Message m2 = new Message(2L, "Hello Jersey", "assir");
-
-        List<Message> messages = new ArrayList<>();
-        messages.add(m1);
-        messages.add(m2);
-        return messages;
+    public MessageService() {
+        messages.put(1L, new Message(1, "Hello world", "assir"));
+        messages.put(2L, new Message(2, "Hello jersey", "assir"));
     }
+
+    public List<Message> getAllMessages() {
+        return new ArrayList<>(messages.values());
+    }
+
+    public Message getMessage(long id) {
+        return messages.get(id);
+    }
+
+    public Message addMessage(Message message) {
+        message.setId(messages.size() + 1);
+        messages.put(message.getId(), message);
+        return message;
+    }
+
+    public Message updateMessage(Message message) {
+        if (message.getId() <= 0) {
+            return null;
+        }
+        messages.put(message.getId(), message);
+        return message;
+    }
+
+    public Message removeMessage(long id) {
+        return messages.remove(id);
+    }
+
 }
