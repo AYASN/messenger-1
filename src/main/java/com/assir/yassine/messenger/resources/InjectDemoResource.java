252ef59c59d74d2c234acd1601624c5507dea9f3
AYassine <assiryassine@gmail.com>
2019-11-12 16:12:00 +0100

    Use Context and BeanParam annotations

--- src/main/java/com/assir/yassine/messenger/resources/InjectDemoResource.java
+++ src/main/java/com/assir/yassine/messenger/resources/InjectDemoResource.java
@@ -1,7 +1,10 @@
 package com.assir.yassine.messenger.resources;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/injectdemo")
 @Consumes(MediaType.TEXT_PLAIN)
@@ -9,11 +12,21 @@ import javax.ws.rs.core.MediaType;
 public class InjectDemoResource {
 
     @GET
-    @Path("/annotations")
+    @Path("annotations")
     public String getParamsUsingAnnotations(@MatrixParam("param") String matrixParam,
                                             @HeaderParam("authSessionID") String header,
                                             @CookieParam("JSESSIONID") String cookie) {
         return "Matrix param: " + matrixParam + ", header param: " + header +
                 " and cookie: " + cookie;
     }
+
+    @GET
+    @Path("context")
+    public String getParamsUsingContext(@Context UriInfo uriInfo, @Context HttpHeaders headers) {
+
+        String path = uriInfo.getAbsolutePath().toString();
+        String cookies = headers.getCookies().toString();
+        return "Path: " + path + ", cookies: " + cookies;
+    }
+
 }
--- src/main/java/com/assir/yassine/messenger/resources/MessageResource.java
+++ src/main/java/com/assir/yassine/messenger/resources/MessageResource.java
@@ -2,6 +2,7 @@ package com.assir.yassine.messenger.resources;
 
 
 import com.assir.yassine.messenger.model.Message;
+import com.assir.yassine.messenger.resources.beans.MessageFilterBean;
 import com.assir.yassine.messenger.service.MessageService;
 
 import javax.ws.rs.*;
@@ -16,14 +17,13 @@ public class MessageResource {
     MessageService messageService = new MessageService();
 
     @GET
-    public List<Message> getMessages(@QueryParam("year") int year,
-                                     @QueryParam("start") int start,
-                                     @QueryParam("size") int size) {
-        if (year > 0) {
-            return messageService.getAllMessagesForYear(year);
+    public List<Message> getMessages(@BeanParam MessageFilterBean filterBean) {
+        if (filterBean.getYear() > 0) {
+            return messageService.getAllMessagesForYear(filterBean.getYear());
         }
-        if (start >= 0 && size > 0) {
-            return messageService.getAllMessagesPaginated(start, size);
+        if (filterBean.getStart() >= 0 && filterBean.getSize() > 0) {
+            return messageService.
+                    getAllMessagesPaginated(filterBean.getStart(), filterBean.getSize());
         }
         return messageService.getAllMessages();
     }
