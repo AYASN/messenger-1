18f63de870c6c1db410267c2c75c595cfa4ef571
AYassine <assiryassine@gmail.com>
2019-11-13 22:11:34 +0100

    Use JAX-RS WebApplicationException to handle exceptions

--- src/main/java/com/assir/yassine/messenger/exception/GenericExceptionMapper.java
+++ src/main/java/com/assir/yassine/messenger/exception/GenericExceptionMapper.java
@@ -6,7 +6,6 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-@Provider
 public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
 
     @Override
